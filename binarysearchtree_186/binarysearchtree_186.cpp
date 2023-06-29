54d43afaa2a904959cfbc02b883ca816666d993f
cahyadie <cahyadiilham12@gmail.com>
2023-06-29 17:54:51 +0700

    mengatur current node

--- binarysearchtree_186/binarysearchtree_186.cpp
+++ binarysearchtree_186/binarysearchtree_186.cpp
@@ -51,5 +51,17 @@ public:
 		}
 	}
 
+	void search(string element, Node*& parent, Node*& currentNode) { //tis function search the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
 
+		}
+	}
 };  
